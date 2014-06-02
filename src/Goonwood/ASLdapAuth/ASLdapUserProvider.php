ffccb7ed6ce9e67b3fc7e5636ab37ad2bb66f007
Sam Greenwood <sam@goonbook.goonwood.wan>
2014-06-03 00:43:14 +0930

    updates to work with laravel 4.1

--- src/Goonwood/ASLdapAuth/ASLdapUserProvider.php
+++ src/Goonwood/ASLdapAuth/ASLdapUserProvider.php
@@ -6,21 +6,51 @@ use Illuminate\Auth\UserInterface;
 class ASLDAPUserProvider implements UserProviderInterface {
 
 	protected $conn;
-	protected $dn;
-	protected $filter;
 
+	/**
+	 * Create a new instance of the ASLdapUserProvider
+	 * When this is created we want to establish an update the the Air-Stream LDAP Server
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		$this->conn = ldap_connect("ldaps://mail.air-stream.org", 636);
 	}
 
-	
-	public function retrieveByID($identifier)
-	{
-		return $identifier;
-	}	
-	
-	public function retrieveByCredentials(array $credentials)
+	/**
+	 * Retrieve a user by their unique identifier.
+	 *
+	 * @param  mixed  $identifier
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
+	public function retrieveById($identifier) {}
+
+	/**
+	 * Retrieve a user by by their unique identifier and "remember me" token.
+	 *
+	 * @param  mixed  $identifier
+	 * @param  string  $token
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
+	public function retrieveByToken($identifier, $token) {}
+
+	/**
+	 * Update the "remember me" token for the given user in storage.
+	 *
+	 * @param  \Illuminate\Auth\UserInterface  $user
+	 * @param  string  $token
+	 * @return void
+	 */
+	public function updateRememberToken(UserInterface $user, $token) {}
+
+	/**
+	 * Retrieve a user by the given credentials.
+	 *
+	 * @param  array  $credentials
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
+	public function retrieveByCredentials(array $credentials) 
 	{
 		if($this->bind($credentials))
 		{
@@ -42,11 +72,25 @@ class ASLDAPUserProvider implements UserProviderInterface {
 		}
 	}
 
+	/**
+	 * Validate a user against the given credentials.
+	 *
+	 * @param  \Illuminate\Auth\UserInterface  $user
+	 * @param  array  $credentials
+	 * @return bool
+	 */
 	public function validateCredentials(UserInterface $user, array $credentials)
 	{
 		return $this->bind($credentials);
 	}
 
+
+	/**
+	 * Bind to the LDAP server using the given credentials
+	 * 
+	 * @param  array  $credentials
+	 * @return bool
+	 */
 	private function bind(array $credentials)
 	{
 		$username = "uid=".$credentials['username'].",ou=people,dc=air-stream,dc=org";
